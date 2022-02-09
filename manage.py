9b7169233818e12874a158325b1773ab7b64a2a7
davekennewell <davekennewell@gmail.com>
2022-02-09 18:22:22 +0400

    read settings from environment file

--- manage.py
+++ manage.py
@@ -2,20 +2,28 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import re
 
 
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compute.settings')
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "compute.settings")
     try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        with open('/etc/systemd/compute_environment.conf') as f:
+            content = f.read()
+    except IOError:
+        content = ''
+
+    for line in content.splitlines():
+        m1 = re.match(r'\A([A-Za-z_0-9]+)=(.*)\Z', line)
+        if m1:
+            key, val = m1.group(1), m1.group(2)
+            m2 = re.match(r"\A'(.*)'\Z", val)
+            if m2:
+                val = m2.group(1)
+            m3 = re.match(r'\A"(.*)"\Z', val)
+            if m3:
+                val = re.sub(r'\\(.)', r'\1', m3.group(1))
+            os.environ.setdefault(key, val)
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
