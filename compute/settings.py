f54d6d184d09253e76738fff683f3709d68e574d
davekennewell <davekennewell@gmail.com>
2022-02-09 19:03:26 +0400

    fix static_root bug

--- compute/settings.py
+++ compute/settings.py
@@ -117,5 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.environ.get('STATIC_ROOT', '/opt/data/static_root/'),
-MEDIA_ROOT = os.environ.get('MEDIA_ROOT', '/opt/data/uploaded/'),
+STATIC_ROOT = os.environ.get('STATIC_ROOT', '/opt/data/static_root/')
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', '/opt/data/uploaded/')
