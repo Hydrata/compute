8f9f57f75f091af8970e3481aba45081f79ad7e2
davekennewell <davekennewell@gmail.com>
2022-02-09 17:16:27 +0400

    added a fallback key for testing

--- compute/settings.py
+++ compute/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
