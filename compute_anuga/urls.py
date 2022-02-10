0c6f7f9f3423753394451b947e58a9868cebe598
davekennewell <davekennewell@gmail.com>
2022-02-10 12:42:25 +0400

    communicate between hydrata and compute machine

--- compute_anuga/urls.py
+++ compute_anuga/urls.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
 from django.conf.urls import url, include
-from compute_anuga import api
+from compute_anuga import api, views
 
 router = routers.DefaultRouter()
 router.register(r'compute', api.ComputeViewSet, 'compute')
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^api/', include(router.urls)),
  ]
 
